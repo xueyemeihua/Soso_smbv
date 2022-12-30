cc8a6a472e78e9f37bc9a56ea5885f9612837929
ly <2889533234@qq.com>
2022-12-30 16:05:15 +0800

    添加了展示个人业务功能

--- src/main/java/com/sky/soso/service/BusinessService.java
+++ src/main/java/com/sky/soso/service/BusinessService.java
@@ -9,9 +9,6 @@ import java.util.List;
 
 public class BusinessService {
 
-
-
-
     public List<Business> showMyBusiness(Integer loginUserUid) {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         List<Business> myBuss = sqlSession.getMapper(BusinessMapper.class).getMyBusinessByUid(loginUserUid);
